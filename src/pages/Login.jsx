a1c1529678faffef89f32ff7bc0cb2230824079d
ibrahimajifajarromadhon <ajii4262@gmail.com>
2023-12-25 12:37:03 +0700

    masuk

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 // import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import GoogleLogin from "../components/GoogleLogin";
+import Cookies from "js-cookie";
 
 function Login() {
   // const navigate = useNavigate();
@@ -34,8 +35,8 @@ function Login() {
       const response = await axios.request(config);
       console.log((response))
       const { token } = response.data.data;
-
-      localStorage.setItem("token", token);
+      Cookies.set("token", token, { httpOnly: true });
+      // localStorage.setItem("token", token);
 
       // navigate("/");
 
